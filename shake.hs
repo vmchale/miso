d189310d973cd480cbb13b15a3f2ee9f3bc5151a
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-15 13:45:14 -0500

    Patch up shake.hs

--- shake.hs
+++ shake.hs
@@ -1,44 +1,19 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, shake, shake-cabal, shake-google-closure-compiler, shake-ext, directory
+build-depends: base, shake, shake-cabal, shake-google-closure-compiler, directory
 default-language: Haskell2010
+ghc-options: -Wall
 -}
 
-import           Control.Monad
-import           Development.Shake                 hiding (getEnv)
+import           Development.Shake     
 import           Development.Shake.Cabal
-import           Development.Shake.Clean
 import           Development.Shake.ClosureCompiler
 import           Development.Shake.FilePath
 import           System.Directory
-import           System.Environment                (getEnv)
 
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = ".shake", shakeLint = Just LintBasic, shakeChange = ChangeModtimeAndDigestInput } $ do
     want [ "target/index.html" ]
-
-    "deploy" ~> do
-        home <- liftIO $ getEnv "HOME"
-        let srcFiles = [ "target/index.html", "target/all.min.js" ]
-            dir = home ++ "/programming/rust/drunk/static/"
-        need srcFiles
-        zipWithM_ copyFile' srcFiles ((dir ++) . takeFileName <$> srcFiles)
-        command [Cwd "/home/vanessa/programming/rust/drunk"] "just" ["deploy"]
-
-    "build" %> \_ -> do
-        need ["shake.hs"]
-        copyFile' "shake.hs" ".shake/shake.hs"
-        command_ [Cwd ".shake"] "ghc" ["-Wall", "-Werror", "-O", "shake.hs", "-o", "build", "-threaded", "-rtsopts", "-with-rtsopts=-I0 -qg -qb"]
-        cmd ["cp", "-f", ".shake/build", "."]
-
-    "size" ~> do
-        need ["target/all.min.js"]
-        cmd ["sn", "d", "target/all.min.js"]
-
-    "clean" ~> do
-        cleanHaskell
-        removeFilesAfter "target" ["//*"]
-        removeFilesAfter ".shake" ["//*"]
 
     [ "dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/{{ project }}-0.1.0.0/x/{{ project }}/opt/build/{{ project }}/{{ project }}.jsexe/all.js", "dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/{{ project }}-0.1.0.0/x/{{ project }}/opt/build/{{ project }}/{{ project }}.jsexe/all.js.externs" ] &%> \_ -> do
         need . snd =<< getCabalDepsA "{{ project }}.cabal"
